eed8295ddec035b718cc990921c97888a5bc4815
Ольга Чернецкая <chernetzkayaolga@yandex.by>
2023-09-01 09:57:48 +0300

    test_routes.py test_contest.py сделано

--- news/pytest_tests/conftest.py
+++ news/pytest_tests/conftest.py
@@ -46,3 +46,28 @@ def new_id_for_args(new):
 @pytest.fixture
 def comment_id_for_args(comment):
     return comment.id,
+
+
+@pytest.fixture
+def news_more_than_on_page():
+    today = datetime.today()
+    all_news = [
+        News(
+            title=f'Новость {index}',
+            text='Просто текст.',
+            date=today - timedelta(days=index)
+        )
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+    ]
+    return News.objects.bulk_create(all_news)
+
+
+@pytest.fixture
+def comments_with_different_dates(new, author):
+    now = timezone.now()
+    for index in range(2):
+        comment = Comment.objects.create(
+            news=new, author=author, text=f'Tекст {index}',
+        )
+        comment.created = now + timedelta(days=index)
+        comment.save()
--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -1,77 +1,45 @@
-from datetime import datetime, timedelta
+import pytest
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
-
-from news.models import Comment, News
 
 
-User = get_user_model()
+@pytest.mark.django_db
+def test_news_count(client, news_more_than_on_page):
+    response = client.get(reverse('news:home'))
+    object_list = response.context['object_list']
+    news_count = len(object_list)
+    assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-class TestContent(TestCase):
-
-    HOME_URL = reverse('news:home')
-
-    @classmethod
-    def setUpTestData(cls):
-        today = datetime.today()
-        all_news = [
-            News(
-                title=f'Новость {index}',
-                text='Просто текст.',
-                date=today - timedelta(days=index)
-            )
-            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-        ]
-        News.objects.bulk_create(all_news)
-
-    def test_news_count(self):
-        response = self.client.get(self.HOME_URL)
-        object_list = response.context['object_list']
-        news_count = len(object_list)
-        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
-
-    def test_news_order(self):
-        response = self.client.get(self.HOME_URL)
-        object_list = response.context['object_list']
-        all_dates = [news.date for news in object_list]
-        sorted_dates = sorted(all_dates, reverse=True)
-        self.assertEqual(all_dates, sorted_dates)
+@pytest.mark.django_db
+def test_news_order(client, news_more_than_on_page):
+    response = client.get(reverse('news:home'))
+    object_list = response.context['object_list']
+    all_dates = [news.date for news in object_list]
+    sorted_dates = sorted(all_dates, reverse=True)
+    assert all_dates == sorted_dates
 
 
-class TestDetailPage(TestCase):
+@pytest.mark.django_db
+def test_comments_order(client, comments_with_different_dates, new_id_for_args):
+    url = reverse('news:detail', args=new_id_for_args)
+    response = client.get(url)
+    assert 'news' in response.context
+    news = response.context['news']
+    all_comments = news.comment_set.all()
+    assert all_comments[0].created < all_comments[1].created
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.news = News.objects.create(
-            title='Тестовая новость', text='Просто текст.'
-        )
-        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
-        cls.author = User.objects.create(username='Комментатор')
-        now = timezone.now()
-        for index in range(2):
-            comment = Comment.objects.create(
-                news=cls.news, author=cls.author, text=f'Tекст {index}',
-            )
-            comment.created = now + timedelta(days=index)
-            comment.save()
 
-    def test_comments_order(self):
-        response = self.client.get(self.detail_url)
-        self.assertIn('news', response.context)
-        news = response.context['news']
-        all_comments = news.comment_set.all()
-        self.assertLess(all_comments[0].created, all_comments[1].created)
+@pytest.mark.django_db
+def test_anonymous_client_has_no_form(client, new_id_for_args):
+    detail_url = reverse('news:detail', args=new_id_for_args)
+    response = client.get(detail_url)
+    assert 'form' not in response.context
 
-    def test_anonymous_client_has_no_form(self):
-        response = self.client.get(self.detail_url)
-        self.assertNotIn('form', response.context)
 
-    def test_authorized_client_has_form(self):
-        self.client.force_login(self.author)
-        response = self.client.get(self.detail_url)
-        self.assertIn('form', response.context)
+@pytest.mark.django_db
+def test_authorized_client_has_form(author_client, new_id_for_args):
+    detail_url = reverse('news:detail', args=new_id_for_args)
+    response = author_client.get(detail_url)
+    assert 'form' in response.context
